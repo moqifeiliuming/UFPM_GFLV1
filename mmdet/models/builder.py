663262ee78533d4475c2ed42a711b7964a924ea1
moqifeiliuming <3218872068@qq.com>
2023-08-17 15:12:21 +0800

    UFPM_GFLV1

---
+++ mmdet/models/builder.py
@@ -0,0 +1,155 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import warnings
+from torch import nn
+# from mmcv.cnn import MODELS as MMCV_MODELS
+from mmcv.utils import Registry,build_from_cfg
+# from modelscope.utils.registry import Registry, build_from_cfg
+from mmcv.cnn import MODELS
+
+# MODELS = Registry('models', parent=MMCV_MODELS)
+# BACKBONES = MODELS
+# NECKS = MODELS
+# ROI_EXTRACTORS = MODELS
+# SHARED_HEADS = MODELS
+# HEADS = MODELS
+# LOSSES = MODELS
+# DETECTORS = MODELS
+
+MODELS= Registry('models')
+BACKBONES = Registry('backbone')
+NECKS = Registry('neck')
+ROI_EXTRACTORS = Registry('roi_extractor')
+SHARED_HEADS = Registry('shared_head')
+HEADS = Registry('head')
+LOSSES = Registry('loss')
+DETECTORS = Registry('detector')
+
+# def build_backbone(cfg):
+#     """Build backbone."""
+#     return BACKBONES.build(cfg)
+#
+#
+# def build_neck(cfg):
+#     """Build neck."""
+#     return NECKS.build(cfg)
+#
+#
+# def build_roi_extractor(cfg):
+#     """Build roi extractor."""
+#     return ROI_EXTRACTORS.build(cfg)
+#
+#
+# def build_shared_head(cfg):
+#     """Build shared head."""
+#     return SHARED_HEADS.build(cfg)
+#
+#
+# def build_head(cfg):
+#     """Build head."""
+#     return HEADS.build(cfg)
+#
+#
+# def build_loss(cfg):
+#     """Build loss."""
+#     return LOSSES.build(cfg)
+#
+#
+# def build_detector(cfg, train_cfg=None, test_cfg=None):
+#     """Build detector."""
+#     if train_cfg is not None or test_cfg is not None:
+#         warnings.warn(
+#             'train_cfg and test_cfg is deprecated, '
+#             'please specify them in model', UserWarning)
+#     assert cfg.get('train_cfg') is None or train_cfg is None, \
+#         'train_cfg specified in both outer field and model field '
+#     assert cfg.get('test_cfg') is None or test_cfg is None, \
+#         'test_cfg specified in both outer field and model field '
+#     return DETECTORS.build(
+#         cfg, default_args=dict(train_cfg=train_cfg, test_cfg=test_cfg))
+#
+# def build_model(cfg, default_args: dict = None):
+#     """ build model given a model name
+#     Args:
+#         name (str, optional):  Model name, if None, default model
+#             will be used.
+#         default_args (dict, optional): Default initialization arguments.
+#     """
+#     return MODELS.build(cfg, default_args=default_args)
+
+def build(cfg, registry, default_args=None):
+    """Build a module.
+
+    Args:
+        cfg (dict, list[dict]): The config of modules, is is either a dict
+            or a list of configs.
+        registry (:obj:`Registry`): A registry the module belongs to.
+        default_args (dict, optional): Default arguments to build the module.
+            Defaults to None.
+
+    Returns:
+        nn.Module: A built nn module.
+    """
+    if isinstance(cfg, list):
+        modules = [
+            build_from_cfg(cfg_, registry, default_args) for cfg_ in cfg
+        ]
+        return nn.Sequential(*modules)
+    else:
+        return build_from_cfg(cfg, registry, default_args)
+
+
+def build_backbone(cfg):
+    """Build backbone."""
+    return build(cfg, BACKBONES)
+
+
+def build_neck(cfg):
+    """Build neck."""
+    return build(cfg, NECKS)
+
+
+def build_roi_extractor(cfg):
+    """Build roi extractor."""
+    return build(cfg, ROI_EXTRACTORS)
+
+
+def build_shared_head(cfg):
+    """Build shared head."""
+    return build(cfg, SHARED_HEADS)
+
+
+def build_head(cfg):
+    """Build head."""
+    return build(cfg, HEADS)
+
+
+def build_loss(cfg):
+    """Build loss."""
+    return build(cfg, LOSSES)
+
+
+def build_detector(cfg, train_cfg=None, test_cfg=None):
+    """Build detector."""
+    return build(cfg, DETECTORS, dict(train_cfg=train_cfg, test_cfg=test_cfg))
+
+def build_model(cfg, default_args: dict = None):
+    """ build model given a model name
+    Args:
+        name (str, optional):  Model name, if None, default model
+            will be used.
+        default_args (dict, optional): Default initialization arguments.
+    """
+    return build_from_cfg(cfg, MODELS, default_args=default_args)
+
+# def build_detector(cfg, train_cfg=None, test_cfg=None):
+#     """Build detector."""
+#     if train_cfg is not None or test_cfg is not None:
+#         warnings.warn(
+#             'train_cfg and test_cfg is deprecated, '
+#             'please specify them in model', UserWarning)
+#     assert cfg.get('train_cfg') is None or train_cfg is None, \
+#         'train_cfg specified in both outer field and model field '
+#     assert cfg.get('test_cfg') is None or test_cfg is None, \
+#         'test_cfg specified in both outer field and model field '
+#     return DETECTORS.build(
+#         cfg, default_args=dict(train_cfg=train_cfg, test_cfg=test_cfg))
